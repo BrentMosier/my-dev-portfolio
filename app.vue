ae556e6eca06ce37450bfec98e32f409f7bc52c3
BrentMosier <brentmosier1@gmail.com>
2024-06-12 20:22:47 -0400

    created simple project structure

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-slate-800 text-white">
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="h-full">
+      <h1>Index page</h1>
+      <button class="bg-cyan-900">Test</button>
+      <div>Root: intro area, about me page</div>
+    </div>
+  </template>
+  
+  <script setup>
+  import {ref} from 'vue'
+  
+  const count = ref(20)
+  </script>
+  
